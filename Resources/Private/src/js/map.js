097f67b7c8f83cde45adf83e0aa1da5a7d09e261
Georg Duemmler <gd@bergwerk.ag>
2016-05-04 12:38:52 +0200

    .

--- Resources/Private/src/js/map.js
+++ Resources/Private/src/js/map.js
@@ -84,6 +84,7 @@ var BwrkMarkerMap = {
     },
     geoCoder: null,
     distanceMatrix: null,
+    geoCodeMarkers: [],
     searchBox: null,
     markers: null,
     init: function () {
@@ -129,28 +130,32 @@ var BwrkMarkerMap = {
     codeAddress: function (address) {
         var that = this;
         this.geoCoder.geocode({
-            'address': address
-        },
-        function (results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                var result = results[0];
-                var nearestMarkerObject = that.measureDistance(result.geometry.location);
-                var nearestMarker = new google.maps.Marker({
-                    position: nearestMarkerObject.position
-                });
-                var locationMarker = new google.maps.Marker({
-                    map: that.map.googleElement,
-                    position: result.geometry.location
-                });
-                var markers = [];
+                'address': address,
+                'region': 'de'
+            },
+            function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    that.setMapOnAll(that.geoCodeMarkers, null);
+
+                    var result = results[0];
+                    var nearestMarkerObject = that.measureDistance(result.geometry.location);
+                    var nearestMarker = new google.maps.Marker({
+                        position: nearestMarkerObject.position
+                    });
+                    var locationMarker = new google.maps.Marker({
+                        map: that.map.googleElement,
+                        position: result.geometry.location
+                    });
+                    that.geoCodeMarkers.push(locationMarker);
+
+                    var markers = [];
                     markers[0] = locationMarker;
                     markers[1] = nearestMarker;
-                that.fitBounds(markers);
-            }
-        });
+                    that.fitBounds(markers);
+                }
+            });
     },
-    measureDistance: function(origin)
-    {
+    measureDistance: function (origin) {
         var lat = origin.lat();
         var lng = origin.lng();
         var R = 6371; // radius of earth in km
@@ -158,27 +163,31 @@ var BwrkMarkerMap = {
         var closest = -1;
         var that = this;
 
-        for( i=0;i < this.markers.length; i++ ) {
+        for (i = 0; i < this.markers.length; i++) {
             var mlat = that.markers[i].position.lat;
             var mlng = that.markers[i].position.lng;
-            var dLat  = that.rad(mlat - lat);
+            var dLat = that.rad(mlat - lat);
             var dLong = that.rad(mlng - lng);
-            var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.cos(that.rad(lat)) * Math.cos(that.rad(lat)) * Math.sin(dLong/2) * Math.sin(dLong/2);
-            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(that.rad(lat)) * Math.cos(that.rad(lat)) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
             var d = R * c;
             distances[i] = d;
-            if ( closest == -1 || d < distances[closest] ) {
+            if (closest == -1 || d < distances[closest]) {
                 closest = i;
             }
         }
         return this.markers[closest];
     },
-    rad: function(x) {
-        return x*Math.PI/180;
+    setMapOnAll: function (markers, map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
     },
-    fitBounds: function(markers)
-    {
+    rad: function (x) {
+        return x * Math.PI / 180;
+    },
+    fitBounds: function (markers) {
         var bounds = new google.maps.LatLngBounds();
         for (var i = 0; i < markers.length; i++) {
             bounds.extend(markers[i].getPosition());
